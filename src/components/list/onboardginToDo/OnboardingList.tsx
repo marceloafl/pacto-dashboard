da4f0412075f0d1c1908e41edcb63be001fa63ea
Marcelo Lemes <marceloaflemes@gmail.com>
2024-08-02 16:12:41 -0300

    feat: improving onboarding list

--- src/components/list/onboardginToDo/OnboardingList.tsx
+++ src/components/list/onboardginToDo/OnboardingList.tsx
@@ -1,22 +1,31 @@
 import { Box, List, ListItem, ListItemIcon } from "@mui/material";
+import {
+  onboardingListData,
+  OnboardingListDataType,
+} from "./OnboardingList.data";
 
-interface IOnboardingList {
-  listItems: { text: string; done: boolean; id: string }[];
+export interface IListItem {
+  type: OnboardingListDataType;
+  done: boolean;
 }
 
-export default function OnboardingList({ listItems }: IOnboardingList) {
+interface IOnboardingListProps {
+  listItems: IListItem[];
+}
+
+export default function OnboardingList({ listItems }: IOnboardingListProps) {
   return (
     <Box className="w-full text-pactto-gray">
       <nav aria-label="main mailbox folders">
         <List className="flex flex-col gap-4">
           {listItems.map((item) => (
-            <ListItem key={item.id} disablePadding className="flex gap-2">
+            <ListItem key={item.type} disablePadding className="flex gap-2">
               <ListItemIcon
                 className={`flex items-center justify-center text-pactto-gray text-white min-w-[38px] h-[18px] rounded-lg font- text-[9px] px-0.5 py-1.5 ${item.done ? "bg-accent" : "bg-gray-500"}`}
               >
                 <p>{item.done ? "DONE" : "TO DO"}</p>
               </ListItemIcon>
-              <p className="font-medium text-sm">{item.text}</p>
+              <OnboardingItemText type={item.type} />
             </ListItem>
           ))}
         </List>
@@ -24,3 +33,7 @@ export default function OnboardingList({ listItems }: IOnboardingList) {
     </Box>
   );
 }
+
+const OnboardingItemText = ({ type }: { type: OnboardingListDataType }) => {
+  return <div>{onboardingListData[type].text}</div>;
+};
