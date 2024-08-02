90386abfb9001e7c395251820c56944811105dc9
Marcelo Lemes <marceloaflemes@gmail.com>
2024-08-02 16:20:52 -0300

    feat: creating button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className=" bg-default-black-bg">{children}</body>
+      <body className=" bg-default-black">{children}</body>
     </html>
   );
 }
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,27 @@
+import { Button as MuiButton, Stack } from "@mui/material/";
+
+export const buttonTheme = {
+  primary: "bg-accent text-white",
+  gray: "bg-pactto-gray text-default-black",
+  blue: "bg-default-blue text-default-black",
+};
+
+interface IButtonProps {
+  children?: React.ReactNode;
+  theme?: keyof typeof buttonTheme;
+  startIcon?: React.ReactNode;
+}
+
+export default function Button({ theme, children, startIcon }: IButtonProps) {
+  return (
+    <Stack direction="row" spacing={2}>
+      <MuiButton
+        variant="outlined"
+        startIcon={startIcon}
+        className={`font-semibold border-0 rounded-3xl w-full h-12 ${buttonTheme[theme]}`}
+      >
+        {children}
+      </MuiButton>
+    </Stack>
+  );
+}
